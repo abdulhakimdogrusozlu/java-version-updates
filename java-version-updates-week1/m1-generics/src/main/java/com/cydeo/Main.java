26258cc6398b31f82373daaba6a44751cb07bae9
abdulhakim dogrusozlu <abdulhakimdogrusozlu@gmail.com>
2023-07-17 12:28:43 +0300

    Generics

--- java-version-updates-week1/m1-generics/src/main/java/com/cydeo/Main.java
+++ java-version-updates-week1/m1-generics/src/main/java/com/cydeo/Main.java
@@ -1,7 +1,29 @@
 package com.cydeo;
 
+import java.util.ArrayList;
+
 public class Main {
 
+    public static void main(String[] args) {
+
+        ArrayList<Integer> items = new ArrayList<Integer>();
+
+        items.add(1);
+        items.add(2);
+        items.add(3);
+        items.add(4);
+
+        printDoubled(items);
+
+
+
+    }
+
+    private static void printDoubled(ArrayList<Integer> items) {
+        for (Integer i : items){
+            System.out.println(i*2);
+        }
+    }
 
 
 }
